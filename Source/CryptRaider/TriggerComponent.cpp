db994510da03795d98f37383173bc978b2ec9108
Ahmad Dwayke <ahmad.dwayke@icloud.com>
2024-09-09 14:18:14 +0300

    trigger component

---
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TriggerComponent.h"
+
+UTriggerComponent::UTriggerComponent() 
+{
+
+}
+
+void UTriggerComponent::BeginPlay() 
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Display, TEXT("Alive!"));
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) 
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
